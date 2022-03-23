be4a7479e41d6b8a376710e5cb3cc9a00141d08e
kca2250 <2250katok@gmail.com>
2022-03-23 10:37:23 +0900

    atCoder 211

--- 211/BloodPressure.dart
+++ 211/BloodPressure.dart
@@ -2,10 +2,10 @@ import 'dart:io';
 
 void main() {
   // ABの標準入力
-  final AB = stdin.readLineSync()!.split(" ").map(int.parse);
-  final A = AB.first;
-  final B = AB.last;
+  final Iterable<int> AB = stdin.readLineSync()!.split(" ").map(int.parse);
+  final int A = AB.first;
+  final int B = AB.last;
 
-  final C = ((A - B) / 3) + B;
+  final double C = ((A - B) / 3) + B;
   print(C);
 }
